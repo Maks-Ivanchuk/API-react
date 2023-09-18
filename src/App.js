4a14457496c6e266020d7199ca90857e895d24d5
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-09-18 22:38:39 +0300

    something

--- src/App.js
+++ src/App.js
@@ -34,26 +34,14 @@ import './App.css';
 //    );
 // };
 
-const UserButtonTodos = ({ userId, onButtonClick }) => {
-   return (
-      <button className="btn-todos button button--danger" onClick={() => onButtonClick(userId)}>Todos</button>
-   );
-};
 
-const UserButtonPosts = ({ userId, onButtonClick }) => {
-   return (
-      <button className="btn-todos button button--info" onClick={() => onButtonClick(userId)}>Posts</button>
-   );
-};
+function App() { 
 
-const UserButtonAlbums = ({ userId, onButtonClick }) => {
-   return (
-      <button className="btn-todos button button button--warning" onClick={() => onButtonClick(userId)}>Albums</button>
-   );
-};
-
-
-function App() {
+   const UserButtonTodos = ({ userId, onButtonClick }) => {
+      return (
+         <button className="btn-todos button button--danger" onClick={() => onButtonClick(userId)}>Todos</button>
+      );
+   };
 
    const [users, setUsers] = useState([]);
 
@@ -63,77 +51,67 @@ function App() {
          .then(users => setUsers(users));
    }, []);
 
-   let userIdDynamic = null;
-   const [userTodos, setUserTodos] = useState([]);
-   const [todosIsOpen, setTodosIsOpen] = useState(false);
-
-   useEffect(() => {
-      fetch(`https://jsonplaceholder.typicode.com/users/${userIdDynamic}/todos`)
-         .then(response => response.json())
-         .then(todosUser => setUserTodos(todosUser));
-   }, []);
+   // let targetUserId = null;
 
    const clickButtonTodos = (userId) => {
-      userIdDynamic = userId;
-      setTodosIsOpen(!todosIsOpen);
-      console.log(
-         ususerTodos.map(userTodo => (
-            { user.id }</td >
-         }
-      );
-
-
-};
-
-
-
-
-const clickButtonPosts = userId => {
-   console.log('posts for user:', userId);
-};
-
-const clickButtonAlbums = userId => {
-   console.log('Albums for user:', userId);
-};
-
-
-
-return (
-   <>
-      <table id="tableUsers" width="100%">
-         <tr>
-            <th colspan="9">Users</th>
-         </tr>
-         <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>User name</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Phone</th>
-            <th>Website</th>
-            <th>Company</th>
-            <th>Action</th>
-         </tr>
-         {users.map(user => (
+      return (
+         <>
+         <table id="result1">
             <tr>
-               <td>{user.id}</td>
-               <td>{user.name}</td>
-               <td>{user.username}</td>
-               <td>{user.email}</td>
-               <td>{user.address.city}</td>
-               <td>{user.phone}</td>
-               <td>{user.website}</td>
-               <td>{user.company.name}</td>
-               <td>
-                  <UserButtonTodos userId={user.id} onButtonClick={clickButtonTodos} />
-                  <UserButtonPosts userId={user.id} onButtonClick={clickButtonPosts} />
-                  <UserButtonAlbums userId={user.id} onButtonClick={clickButtonAlbums} />
-               </td>
+               <th colspan="4" style={{ color: 'red' }}>Todos user {userId}</th>
             </tr>
-         ))}
-      </table>
-   </>)
+            <tr>
+               <th>User id</th>
+               <th>Id</th>
+               <th>Title</th>
+               <th>Completed</th>`
+               </tr>
+            </table>
+         </>
+      );
+   };
+   
+
+   return (
+      <>
+         <div id="table-wrapper">
+            <table id="tableUsers" width="100%">
+               <tr>
+                  <th colspan="9">Users</th>
+               </tr>
+               <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>User name</th>
+                  <th>Email</th>
+                  <th>Address</th>
+                  <th>Phone</th>
+                  <th>Website</th>
+                  <th>Company</th>
+                  <th>Action</th>
+               </tr>
+               {users.map(user => (
+                  <tr>
+                     <td>{user.id}</td>
+                     <td>{user.name}</td>
+                     <td>{user.username}</td>
+                     <td>{user.email}</td>
+                     <td>{user.address.city}</td>
+                     <td>{user.phone}</td>
+                     <td>{user.website}</td>
+                     <td>{user.company.name}</td>
+                     <td>
+                        <UserButtonTodos userId={user.id} onButtonClick={clickButtonTodos} />
+                        <UserButtonTodos userId={user.id} onButtonClick={clickButtonTodos} />
+                        <UserButtonTodos userId={user.id} onButtonClick={clickButtonTodos} />
+                     </td>
+                  </tr>
+               ))}
+            </table>
+            {clickButtonTodos()}
+         </div>
+      </>
+   )
 };
 
 export default App;
