f96a4f476bd73fd4e4baa7fd0c28139d366d3496
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-09-24 14:20:15 +0300

    sd

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function ButtonTodos({ sign, onBtnClick }) {
+// function ButtonTodos({ sign, onBtnClick }) {
 
+//    return (
+//       <>
+//          <button className="test-btn" onClick={() => onBtnClick(sign)}>Counter {sign}</button>
+//       </>
+//    )
+// }
+
+
+// function App() {
+//    const [counterBtnClick, setCounterBtnClick] = useState(0);
+
+//    const BtnCounterClick = (sign) => {
+//       if (sign === '+') {
+//          setCounterBtnClick(prevCounter => prevCounter + 1);
+//       } else if (sign === '-') {
+//          setCounterBtnClick(prevCounter => prevCounter - 1);
+//       }
+//    }
+
+//    return (
+//       <div className='test'>
+//          <ButtonTodos sign='+' onBtnClick={BtnCounterClick} />
+//          <p>{counterBtnClick}</p>
+//          <ButtonTodos sign='-' onBtnClick={BtnCounterClick} />
+//       </div>
+//    )
+// };
+
+function Button({message, children}) {
    return (
-      <>
-         <button className="test-btn" onClick={() => onBtnClick(sign)}>Counter {sign}</button>
-      </>
+      <button onClick={() => alert(message)}>
+         {children}
+      </button>
    )
 }
 
-
 function App() {
-   const [counterBtnClick, setCounterBtnClick] = useState(0);
-
-   const BtnCounterClick = (sign) => {
-      if (sign === '+') {
-         setCounterBtnClick(prevCounter => prevCounter + 1);
-      } else if (sign === '-') {
-         setCounterBtnClick(prevCounter => prevCounter - 1);
-      }
-   }
-
    return (
-      <div className='test'>
-         <ButtonTodos sign='+' onBtnClick={BtnCounterClick} />
-         <p>{counterBtnClick}</p>
-         <ButtonTodos sign='-' onBtnClick={BtnCounterClick} />
-      </div>
+      <>
+      <Button message='You stupid asshole'>
+         Don`t touch me!
+      </Button>
+      <Button message='I am'>
+            
+            <h1>Are you stupid asshole</h1>
+      </Button>
+      
+      </>
    )
 };
 
