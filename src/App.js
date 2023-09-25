e0e4a43897aa0402436d94dac44ae2b95794a6f4
Maks Ivanchuk <maksivanchu@gmail.com>
2023-09-25 16:31:50 +0300

    sasa

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import './App.css';
 //import { isEditable } from '@testing-library/user-event/dist/utils';
 
 function App() {
-   
-   function Button({ onClick, children, userId, className }) {
+
+   function Button({ onClick, children, userId, className, typeRequest }) {
 
       return (
-         <button className={className} onClick={() => onClick(userId)}>
+         <button className={className} onClick={() => onClick(userId, typeRequest)}>
             {children}
          </button>
       );
    };
-   const [currentUserId, setCurrentUserId] = useState('');
-   const [res1IsOpen, setRes1IsOpen] = useState(false);
-   let displayStyle = res1IsOpen ? { display:'inline-table'} : { display : 'none'};
 
-   function handleOpenTodos(userId) {
+   const [currentUserId, setCurrentUserId] = useState('');
+   const [currentTypeRequest, setCurrentTypeRequest] = useState('');
+   const [res1IsOpen, setRes1IsOpen] = useState(false);
+   const displayStyle = res1IsOpen ? { display: 'inline-table' } : { display: 'none' };
+
+   function handleOpenTodos(userId, typeRequest) {
+      setCurrentTypeRequest(typeRequest);
       setCurrentUserId(userId);
       if (res1IsOpen === false) {
          setRes1IsOpen(!res1IsOpen)
@@ -24,7 +27,7 @@ function App() {
    }
 
    const [users, setUsers] = useState([]);
-   
+
    useEffect(() => {
       fetch('https://jsonplaceholder.typicode.com/users')
          .then(response => response.json())
@@ -40,7 +43,7 @@ function App() {
 
       fetch(`https://jsonplaceholder.typicode.com/users/${currentUserId}/todos`)
          .then(response => response.json())
-         .then(todos => { 
+         .then(todos => {
             if (Object.keys(todos).length > 0) {
                setTodos(todos);
             } else {
@@ -48,10 +51,49 @@ function App() {
             }
          })
    }, [currentUserId]);
-   
+
+   function Result1({ todos }) {
+      if (currentTypeRequest === 'todos') {
+         return (
+            <table id="result1" style={displayStyle}>
+               <thead>
+                  <tr>
+                     <th colSpan="4" style={{ color: 'red' }}>Todos user: {currentUserId}</th>
+                  </tr>
+                  <tr>
+                     <th>User id</th>
+                     <th>Id</th>
+                     <th>Title</th>
+                     <th>Completed</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {todos.map(todo => (
+                     <tr key={todo.id}>
+                        <td>{todo.userId}</td>
+                        <td>{todo.id}</td>
+                        <td>{todo.title}</td>
+                        <td>{todo.completed.toString()}</td>
+                     </tr>
+                  ))}
+               </tbody>
+            </table>
+         );
+      } else if (currentTypeRequest === 'posts') {
+         return (
+            <h1>Posts</h1>
+         )
+      } else if (currentTypeRequest === 'albums') {
+         return (
+            <h1>Albums</h1>
+         )
+      };
+
+   };
+
    return (
       <div id="table-wrapper">
-         <table id="tableUsers" width="100%">
+         <table id="tableUsers">
             <thead>
                <tr>
                   <th colSpan="9">Users</th>
@@ -76,15 +118,21 @@ function App() {
                      <td>{user.address.city}</td>
                      <td>{user.phone}</td>
                      <td>
-                        <Button className={"btn-todos button button--danger"} onClick={handleOpenTodos} userId={user.id}>
-                           Todos user # {user.id}
+                        <Button className={"btn-todos button button--danger"} onClick={handleOpenTodos} userId={user.id} typeRequest={'todos'}>
+                           Todos
+                        </Button>
+                        <Button className={"btn-posts button button--info"} onClick={handleOpenTodos} userId={user.id} typeRequest={'posts'}>
+                           Posts
+                        </Button>
+                        <Button className={"btn-albums button button--warning"} onClick={handleOpenTodos} userId={user.id} typeRequest={'albums'}>
+                           Albums
                         </Button>
                      </td>
                   </tr>
                ))}
             </tbody>
          </table>
-         <table id="result1" style={displayStyle}>
+         {/* <table id="result1" style={displayStyle}>
             <thead>
                <tr>
                   <th colSpan="4" style={{ color: 'red' }}>Todos user: {currentUserId}</th>
@@ -106,7 +154,8 @@ function App() {
                   </tr>
                ))}
             </tbody>
-         </table>
+         </table> */}
+         <Result1 todos={todos} />
       </div>
    )
 };
